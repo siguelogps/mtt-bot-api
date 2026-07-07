b3c06aff829a2c3a9299676c918a6eebd2bfe20d
siguelogps <pablo@siguelogps.cl>
2026-07-07 18:38:03 -0400

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 echo "Instalando dependencias de Python..."
 pip install -r requirements.txt
 
-echo "Instalando Google Chrome..."
+echo "Forzando la descarga de Google Chrome..."
 STORAGE_DIR=/opt/render/project/.render
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "...Descargando Chrome"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
-else
-  echo "...Usando Chrome desde el caché"
-fi
+
+# El botón nuclear del caché: borramos la carpeta corrupta
+rm -rf $STORAGE_DIR/chrome
+
+# Creamos la carpeta limpia y descargamos
+mkdir -p $STORAGE_DIR/chrome
+cd $STORAGE_DIR/chrome
+wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+rm ./google-chrome-stable_current_amd64.deb
